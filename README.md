8fcbe3c1d6d9d79a46cedc4433b717da79dcdc6a
Yldemaro Jimenez <ylde198989@gmail.com>
2020-03-04 12:44:24 -0400

    subida de archivos al firebase

---
+++ README.md
@@ -0,0 +1,4 @@
+# Concurrent Firebase File Uploads | Angular + Firebase 
+
+[Upload Multiple Files to Firebase Storage](https://fireship.io/lessons/angular-firebase-storage-uploads-multi)
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+// Instructions ---->
+// Replace configPlaceholder with your firebase credentials
+import { DropzoneDirective } from './dropzone.directive';
+import { UploaderComponent } from './uploader/uploader.component';
+import { UploadTaskComponent } from './upload-task/upload-task.component';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [AppComponent, DropzoneDirective, UploaderComponent, UploadTaskComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
